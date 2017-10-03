eea0067d9835eea3f95ffa5c041130e50bed2b19
Jacky Yao <blackdragon.yao@gmail.com>
2017-10-03 09:49:10 -0400

    Fixing

--- CollisionSimulation/Ball.cs
+++ CollisionSimulation/Ball.cs
@@ -1,8 +1,17 @@
 ﻿namespace CollisionSimulation
 {
-    int hi = 6;
     public class Ball
     {
-        
+        double vx,vy, posx, posy, ax, ay, radius;
+        public Ball(double p1, double p2, double v1, double v2, double a1, double a2, double r )
+        {
+            this.vx = v1;
+            this.vy = v2;
+            this.posx = p1;
+            this.posy = p2;
+            this.ax = a1;
+            this.ay = a2;
+            this.radius = r;
+        }
     }
 }
